06803034c51145c014446dad5308d997109b05f8
Danila Simanok <danilasimanok@gmail.com>
2026-08-26 19:51:05 +0300

    feat(SimpleEncryption/XorEncoder): XorEncoder ready

--- SimpleEncryption/XorEncoder.cs
+++ SimpleEncryption/XorEncoder.cs
@@ -2,10 +2,10 @@ namespace SimpleEncryption;
 
 public class XorEncoder(Byte key) : IEncoderStrategy
 {
-    private Byte _key = key;
+    private readonly Byte _key = key;
 
     public Byte Transform(Byte b)
     {
-        return 0b00000001;
+        return (byte) (_key ^ b);
     }
 }

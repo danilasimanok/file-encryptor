3191aaf36d5e506b9986ece4e060eacdc46f137e
Danila Simanok <danilasimanok@gmail.com>
2026-08-31 17:11:47 +0300

    refactor(FileTransformer): FileTransformer made MVVM project
    Xor and Caesar inputs constrained

--- FileTransformer/Program.cs
+++ FileTransformer/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
+using System;
 
 namespace FileTransformer;
 
-class Program
+sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized

6b65ee9a9a3d47b7513f392ec54ea0e9c43e05a1
Danila Simanok <danilasimanok@gmail.com>
2026-09-02 20:03:13 +0300

    feat(FileTransformer): Transformer finally transforms

--- FileTransformer/ViewModels/MainViewModel.cs
+++ FileTransformer/ViewModels/MainViewModel.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SimpleEncryption;
+using SimpleEncryption.EncoderFactories;
 
 namespace FileTransformer.ViewModels;
 
@@ -28,10 +32,39 @@ public partial class MainViewModel(FilePickerService filePickerService) : Observ
         ChosenFile = _inputFile is null ? String.Empty : _inputFile.LocalPath;
     }
 
+    private IEncoderFactory GetFactory()
+    {
+        return TransformationIndex switch
+        {
+            0 => new XorEncoderFactory(ShortKey),
+            1 => new CaesarEncoderFactory(ShortKey),
+            2 => new VigenereEncoderFactory(LongKey),
+            3 => new VigenereDecoderFactory(LongKey),
+            _ => throw new UnreachableException()
+        };
+
+    }
+
+
     [RelayCommand]
     public async Task ProcessFile()
     {
         _outputFile = await filePickerService.PickFileToSaveAsync();
-        Console.WriteLine($"Transform: {TransformationIndex}, File name: {_outputFile?.LocalPath}");
+
+        var inputStream = File.OpenRead(_inputFile.LocalPath);
+        var outputStream = File.Create(_outputFile.LocalPath);
+        var encoder = GetFactory().CreateEncoder();
+
+        byte[] buffer = new byte[1024];
+        int readLength;
+        while ((readLength = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            for (int i = 0; i < readLength; ++i)
+                buffer[i] = encoder.Transform(buffer[i]);
+            outputStream.Write(buffer, 0, readLength);
+        }
+
+        inputStream.Close();
+        outputStream.Close();
     }
 }
